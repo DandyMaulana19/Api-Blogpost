159c30c14adb2048438bbd8178f84e28cdc2e218
DandyMaulana19 <dandyainul19@gmail.com>
2025-03-09 01:59:49 +0700

    api: adding update endpoint

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -6,6 +6,7 @@ const getPosts = async (req, res) => {
     const posts = await prisma.post.findMany();
 
     return res.json({
+      success: true,
       message: "Posts retrieved successfully",
       statuscode: 200,
       data: posts,
@@ -47,14 +48,73 @@ const createPost = async (req, res) => {
     });
 
     return res.json({
+      success: true,
       message: "Posts created successfully",
       statuscode: 200,
       data: post,
     });
-  } catch (error) {}
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).json({
+      success: false,
+      message: "Error retrieving posts",
+      statuscode: 500,
+      data: null,
+    });
+  }
+};
+
+const updatePost = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const parse = postSchema.safeParse(req.body);
+
+    if (!parse.success) {
+      const errorMessages = parse.error.issues.map(
+        (err) => `${err.path} - ${err.message}`
+      );
+
+      return res.json({
+        message: errorMessages,
+        statuscode: 500,
+        data: null,
+      });
+    }
+
+    const post = await prisma.post.update({
+      where: {
+        id: Number.parseInt(id),
+      },
+      data: {
+        author_name: parse.data.author_name,
+        content: parse.data.content,
+        title: parse.data.title,
+        published: parse.data.published,
+      },
+    });
+
+    return res.json({
+      success: true,
+      message: "Posts updated successfully",
+      statuscode: 200,
+      data: post,
+    });
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).json({
+      success: false,
+      message: "Error updateing posts",
+      statuscode: 500,
+      data: null,
+    });
+  }
 };
 
 module.exports = {
   getPosts,
   createPost,
+  updatePost,
 };
